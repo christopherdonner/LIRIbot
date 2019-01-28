d2d97e83e56072f30902238b1c838fa46a1f850a
Christopher Donner <christopher.donner@gmail.com>
2019-01-28 11:32:37 -0500

    basic framework

--- liri.js
+++ liri.js
@@ -1,4 +1,26 @@
 require("dotenv").config();
+
+var fs = require("fs");
+var axios = require("axios");
+
+var spotifyAuth = require("./keys.js")
+
 var Spotify = require('node-spotify-api');
 var request = require('request');
-var spotify = new Spotify(keys.spotify);
+
+//var spotify = new Spotify(keys.spotify);
+
+console.log(process.argv[2])
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+
+
+fs.readFile("random.txt", "utf8", function(error, data)
+{
+
+})
