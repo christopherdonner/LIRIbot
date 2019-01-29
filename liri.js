811304d490cb3d6f88b3b3fb47cd31bbeb718158
Christopher Donner <christopher.donner@gmail.com>
2019-01-29 11:18:26 -0500

    all but spotify this working

--- liri.js
+++ liri.js
@@ -8,12 +8,22 @@ var spotifyAuth = require("./keys.js")
 var Spotify = require('node-spotify-api');
 var request = require('request');
 
+var logLine=[]
+
+var spotifyClientID="505db673f406470fbab1ec11f47c60ef"
+var spotifyClientSecret="404f95a51fe74453971b96e22238a871"
+
 
 //var spotify = new Spotify(keys.spotify);
 
 console.log(process.argv[2]);
 var action=process.argv[2].toLowerCase().trim();
 console.log(action)
+
+function writeLog(){
+  fs.appendFile("log.txt", logLine, function(err) {
+  })
+  }
 
 /*
 concert-this
@@ -22,6 +32,28 @@ movie-this
 do-what-it-says
 */
 
+
+//concert-this
+if(action==="concert-this"){
+  if(process.argv[3])
+  {
+    var artist=[]
+
+    for(i=3;i<process.argv.length;i++)
+      {
+        artist.push(process.argv[i])
+      }
+      console.log(artist[0])
+      request(`https://rest.bandsintown.com/artists/${artist[0]}/events?app_id=codingbootcamp`, function(error, response, body){
+          console.log(body)
+        })
+  }
+  logLine=`${action}: ${artist}`
+  console.log(`logLine: ${logLine}`)
+  writeLog();
+}
+
+//move-this
 if(action==="movie-this"){
   if(process.argv[3])
   {
@@ -32,22 +64,26 @@ if(action==="movie-this"){
         title.push(process.argv[i])
       }
     console.log(title)
-    // axios.get(`http://www.omdbapi.com/?t="${title}"&y=&plot=short&apikey=trilogy`).then(
-    //   function(response) 
-    //   {
-    //     // Then we print out the imdbRating
-    //     console.log(response.data);
-    //   });
     request(`http://www.omdbapi.com/?t="${title}"&y=&plot=short&apikey=trilogy`, function(error, response, body){
-        console.log(body)
-        var omdbResponse=JSON.parse(body)
-        console.log(omdbResponse)
-        console.log(omdbResponse.Title)
+        console.log(body);
+        var omdbResponse=JSON.parse(body);
+        console.log(omdbResponse);
+        console.log(omdbResponse.Title);
     })
+    writeLog();
   }
 }
 
-fs.readFile("random.txt", "utf8", function(error, data)
+//do-what-it-says
+fs.readFile("random.txt", "utf8", function(err, data)
 {
-
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data)
 })
+
+
+  // If the code experiences any errors it will log the error to the console.
+
+
